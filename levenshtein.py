65bbf364278d0cadd2d2a3b33bab80925f9a0b07
Luiz Felix <lzcfelix@gmail.com>
2015-06-06 10:23:28 +1000

    Experiments

--- levenshtein.py
+++ levenshtein.py
@@ -21,6 +21,7 @@ def lev(s1, s2, i, j):
 
 
 def lev_dp(s1, s2):
+
     tam1 = len(s1)
     tam2 = len(s2)
 
@@ -29,25 +30,78 @@ def lev_dp(s1, s2):
     M[:, 0] = np.array([i for i in range(tam1 + 1)])
     M[0, :] = np.array([i for i in range(tam2 + 1)])
 
+    # computes the edits distance matrix using DP
     for i in range(1, tam1 + 1):
         for j in range(1, tam2 + 1):
-
-            # if s1[i - 1] == s2[j - 1]:
-            #     diff = 0
-            # else:
-            #     diff = 1
-
             M[i, j] = min([
                 (s1[i - 1] != s2[j - 1]) + M[i - 1, j - 1],
                 M[i, j - 1] + 1,
                 M[i - 1, j] + 1
             ])
 
-    return M[tam1, tam2]
+    # Backtracks it to find the edit operations
+    edits1 = edits2 = list()
+
+    
+
+    i, j = tam1 - 1, tam2 - 1
+    while i >= 0 and j >= 0:
+
+        """
+           Discovers what kind of operation was performed:
+             0 = replacing or leaving
+             1 = deletion from s1
+             2 = deletion from s2
+        """
+        op_type = np.argmin([
+            (s1[i - 1] != s2[j - 1]) + M[i - 1, j - 1],
+            M[i, j - 1] + 1,
+            M[i - 1, j] + 1
+        ])
+
+        if op_type == 0:
+            i -= 1
+            j -= 1
+            print 'a'
+
+            print 'Diagonal %c, %c @ %d %d' % (s1[i], s2[j], i, j)
+            if s1[i] == s2[j]:
+                edits1.extend(s1[i])
+                edits2.extend(s2[j])
+            else:
+                # if the chars aren't equal, then it is a substitution. Signalizes this surrounding the char with []
+                # edits1.extend(('[', s1[i], ']'))
+                # edits2.extend(('[', s2[j], ']'))
+
+                edits1.extend((s1[i]))
+                edits2.extend((s2[j]))
+        elif op_type == 1:
+            j -= 1
+
+            print 'b'
+            print 'Left %c, %c @ %d %d' % (s1[i], s2[j], i, j)
+
+            edits1.append('-')
+            edits2.append(s2[j])
+
+        else:
+            i -= 1
+
+            print 'c'
+            print 'Up %c, %c @ %d %d' % (s1[i], s2[j], i, j)
+
+            edits1.append(s1[i])
+            edits2.append('-')
+
+    return M[tam1, tam2], edits1, edits2
 
 
 s1 = "Seven"
 s2 = "Evening"
 s1, s2 = s2, s1
 
-print lev_dp(s1, s2)
+
+edits, e1, e2 = lev_dp(s1, s2)
+print edits
+print ''.join(e1)
+print ''.join(e2)
