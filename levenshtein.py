bf769f70ad765fe77be8dec59d8416c813102518
Luiz Felix <lzcfelix@gmail.com>
2015-06-06 12:13:38 +1000

    Finished Levenshtein Distance DP

--- levenshtein.py
+++ levenshtein.py
@@ -4,7 +4,17 @@ import numpy as np
 
 
 def lev(s1, s2, i, j):
-    print (i, j)
+    """
+    Computes the recursive Levenshtein Distance. Deprecated by lev_dp().
+
+    :param s1: The first string to be used in the comparison (won't be modified)
+    :param s2: The first string to be used in the comparison (won't be modified)
+    :param i: should be len(s1) - 1
+    :param j: should be len(s2) - 1
+
+    :return: The cost for transforming s1 into s2 and vice-versa. The costs for the deletion, replacement and insertion
+    operations is 1.
+    """
 
     if i == 0:
         return j
@@ -21,6 +31,36 @@ def lev(s1, s2, i, j):
 
 
 def lev_dp(s1, s2):
+    """
+    Given two strings, computes the cost of transforming one into other by doing insertions, deletions and changing
+    characters. The costs for all operations is 1 (but this could be changed). This method uses Dynamic Programming.
+
+    :param s1: The first string to be used in the comparison (won't be modified)
+    :param s2: The first string to be used in the comparison (won't be modified)
+
+    :return: A triple (x,y,z) where x is the cost for transforming s1 into s2 (and vice-versa). y contains the
+    modifications that must be made on s1 in order to turn it into s2. Plain characters on y means that this char
+    must remain the same, '-' represents the deletion of the character on the original string and [c] means that the
+    character c in this position was replaced by another character of s2.
+    The same information happens for z in respect of s1.
+    """
+
+    def compare_chars(s1, s2, i, j):
+        """
+        Helper function to compare if two chars in different strings are DIFFERENT. This function exists to avoid the
+        behavior s[-1] == s[len(s) - 1].
+
+        :param s1: The first collection of chars (string).
+        :param s2: The second collection of chars (string).
+        :param i: The position of s1 to be compared to s2
+        :param j: The position of s2 to be compared to s1
+
+        :return: 1 if any index is lesser than 0, otherwise returns 0
+        """
+
+        if i < 0 or j < 0 or s1[i] != s2[j]:
+            return 1
+        return 0
 
     tam1 = len(s1)
     tam2 = len(s2)
@@ -34,74 +74,74 @@ def lev_dp(s1, s2):
     for i in range(1, tam1 + 1):
         for j in range(1, tam2 + 1):
             M[i, j] = min([
-                (s1[i - 1] != s2[j - 1]) + M[i - 1, j - 1],
+                compare_chars(s1, s2, i - 1, j - 1) + M[i - 1, j - 1],
                 M[i, j - 1] + 1,
                 M[i - 1, j] + 1
             ])
 
-    # Backtracks it to find the edit operations
-    edits1 = edits2 = list()
+    edits1 = list()
+    edits2 = list()
 
-    g
-
-    i, j = tam1 - 1, tam2 - 1
-    while i >= 0 and j >= 0:
-
+    i, j = tam1, tam2
+    while i > 0 and j > 0:
         """
-           Discovers what kind of operation was performed:
-             0 = replacing or leaving
-             1 = deletion from s1
-             2 = deletion from s2
+           Discovers what kind of operation was performed (the one with lesser cost):
+             0 = replacing or leaving (diagonal)
+             1 = deletion from s1 (left)
+             2 = deletion from s2 (up)
         """
+
         op_type = np.argmin([
-            (s1[i - 1] != s2[j - 1]) + M[i - 1, j - 1],
+            compare_chars(s1, s2, i - 1, j - 1) + M[i - 1, j - 1],
             M[i, j - 1] + 1,
             M[i - 1, j] + 1
         ])
 
-        if op_type == 0:
-            i -= 1
-            j -= 1
-            print 'a'
+        # print('Appended %d. comparing %c and %c' % (op_type, s1[i - 1], s2[j - 1]))
 
-            print 'Diagonal %c, %c @ %d %d' % (s1[i], s2[j], i, j)
-            if s1[i] == s2[j]:
-                edits1.extend(s1[i])
-                edits2.extend(s2[j])
+        if op_type == 0:
+            if s1[i - 1] == s2[j - 1]:
+                edits1.extend(s1[i - 1])
+                edits2.extend(s2[j - 1])
             else:
                 # if the chars aren't equal, then it is a substitution. Signalizes this surrounding the char with []
-                # edits1.extend(('[', s1[i], ']'))
-                # edits2.extend(('[', s2[j], ']'))
+                # the string will be reversed, that's why the brackets are reversed =)
+                edits1.extend((']', s1[i - 1], '['))
+                edits2.extend((']', s2[j - 1], '['))
 
-                edits1.extend((s1[i]))
-                edits2.extend((s2[j]))
-        elif op_type == 1:
+            i -= 1
             j -= 1
+        elif op_type == 1:
+            edits1.extend('-')
+            edits2.extend(s2[j - 1])
 
-            print 'b'
-            print 'Left %c, %c @ %d %d' % (s1[i], s2[j], i, j)
-
-            edits1.append('-')
-            edits2.append(s2[j])
-
+            j -= 1
         else:
+            edits1.extend(s1[i - 1])
+            edits2.extend('-')
+
             i -= 1
 
-            print 'c'
-            print 'Up %c, %c @ %d %d' % (s1[i], s2[j], i, j)
+    if i > 0 and j <= 0:
+        edits1.extend(s1[0:i])
+        edits2.extend(len(s1[0:i]) * '-')
+    elif j > 0 and i <= 0:
+        edits1.extend(len(s2[0:j]) * '-')
+        edits2.extend(s2[0:j])
 
-            edits1.append(s1[i])
-            edits2.append('-')
-
-    return M[tam1, tam2], edits1, edits2
-
-
-s1 = "Seven"
-s2 = "Evening"
-s1, s2 = s2, s1
+    edits1.reverse()
+    edits2.reverse()
+    return M[tam1, tam2], ''.join(edits1), ''.join(edits2)
 
 
-edits, e1, e2 = lev_dp(s1, s2)
-print edits
-print ''.join(e1)
-print ''.join(e2)
+if __name__ == "__main__":
+
+    s1 = "Evening"
+    s2 = "Seven"
+
+    cost, edt1, edt2 = lev_dp(s1, s2)
+    print edt1
+    print edt2
+    print 'Total cost: %d' % cost
+
+    # print lev(s1, s2, len(s1) - 1, len(s2) - 1)
